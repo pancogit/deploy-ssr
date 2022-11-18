d547a19b9cdc297cb4f00084c8349972c27c5f9a
Panco <panco@pancic.com>
2022-11-18 22:51:41 +0100

    Update environment variables format and app configurations. Add scripts in package.json with environment variables and also add it to the local nodemon debugging script.

--- backend/js/configs.js
+++ backend/js/configs.js
@@ -7,10 +7,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.serverAddress = exports.serverPort = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 // load .env file into process.env
-dotenv_1.default.config();
+// use different environment variables files for development and production mode
+dotenv_1.default.config({ path: `.env.${process.env.NODE_ENV}` });
 // get server configurations
-exports.serverPort = Number(process.env.PORT) || 3000;
-exports.serverAddress = process.env.USE_SERVER_REMOTE_ADDRESS === "true"
-    ? process.env.SERVER_REMOTE_ADDRESS || "0.0.0.0"
-    : process.env.SERVER_LOCAL_ADDRESS || "localhost";
+exports.serverPort = Number(process.env.PORT) || 5000;
+exports.serverAddress = process.env.SERVER_ADDRESS || "localhost";
 //# sourceMappingURL=configs.js.map
--- backend/ts/configs.ts
+++ backend/ts/configs.ts
@@ -3,12 +3,9 @@
 import dotenv from "dotenv";
 
 // load .env file into process.env
-dotenv.config();
+// use different environment variables files for development and production mode
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 // get server configurations
-export const serverPort = Number(process.env.PORT) || 3000;
-
-export const serverAddress =
-    process.env.USE_SERVER_REMOTE_ADDRESS === "true"
-        ? process.env.SERVER_REMOTE_ADDRESS || "0.0.0.0"
-        : process.env.SERVER_LOCAL_ADDRESS || "localhost";
+export const serverPort = Number(process.env.PORT) || 5000;
+export const serverAddress = process.env.SERVER_ADDRESS || "localhost";
